631e66df0e81c8b3736152dabf47d3eaeaf96afd
maverickoo <abc@gmail.com>
2016-10-02 22:17:06 +0000

    fix issues

--- C-projects/Autobuild/AutoConf-Test1/buildRun.sh
+++ C-projects/Autobuild/AutoConf-Test1/buildRun.sh
@@ -1,7 +1,13 @@
 set -x -e
+#### Generate configure.ac ####
+autoreconf -i
+
+### Configure and make ###
+#### Another way to set the target path is setting DESTDIR before running make install ####
 ./configure --prefix=$PWD
 make
 make install
 
+### Run a test ###
 bin/helloWorld
 
--- C-projects/Autobuild/AutoConf-Test1/src/helloWorld.c
+++ C-projects/Autobuild/AutoConf-Test1/src/helloWorld.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
-main()
+int main(int argc, char **argv)
 {
-    printf("Hello World");
-
+    if (argc==1)
+    	printf("Hello World\n");
+    else{
+	int i;
+	for(i=1; i<argc; ++i)
+		printf("%s ", argv[i]);
+	printf("\n");
+	}
+    return 0;
 }
